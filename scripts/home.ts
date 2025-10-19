92742f758b9c316ae9d90a3a9f2095ca65f46993
S-A-N-I-T-Y <justinokpara19@gmail.com>
2025-10-19 13:42:48 +0100

    Change file name

---
+++ scripts/home.ts
@@ -0,0 +1,40 @@
+import type gsapType from "gsap";
+declare const gsap: typeof gsapType;
+declare const Swiper: any;
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const swiperCont = document.querySelector(".mySwiper");
+const valuesSwiper = new Swiper(swiperCont, {
+  speed: 700,
+  resistanceRatio: 0.2,
+  slidesPerView: "auto",
+  grabCursor: true,
+  autoHeight: true,
+  spaceBetween: 24,
+  centeredSlidesBounds: true,
+  keyboard: { enabled: true },
+  observer: true,
+  observerParents: true,
+});
+
+
+const header = document.getElementById("header");
+console.log(header);
+console.log(gsap);
+
+const headerTween = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: header,
+      start: "bottom top",
+    },
+  })
+  .fromTo(
+    header,
+    { backgroundColor: "transparent" },
+    {
+      backgroundColor: "#ffffff",
+      duration: 0.5,
+      ease: "power1.inOut",
+    }
+  );
