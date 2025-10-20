a0a3300072a0c2d011eccdc3a89f4628ce228996
S-A-N-I-T-Y <justinokpara19@gmail.com>
2025-10-20 17:23:44 +0100

    finished landing page

--- dist/home.js
+++ dist/home.js
@@ -2,8 +2,8 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 const swiperCont = document.querySelector(".mySwiper");
 const valuesSwiper = new Swiper(swiperCont, {
     speed: 700,
-    resistanceRatio: 0.2,
     slidesPerView: "auto",
+    resistanceRatio: 0.2,
     grabCursor: true,
     autoHeight: true,
     spaceBetween: 24,
@@ -11,10 +11,33 @@ const valuesSwiper = new Swiper(swiperCont, {
     keyboard: { enabled: true },
     observer: true,
     observerParents: true,
+    breakpoints: {
+        1100: {
+            slidesPerView: 2.5,
+        },
+    },
 });
+const sideNav = document.getElementById("side-nav");
+const hamburger = document.getElementById("menu-bar");
+const closeButton = document.getElementById("close-button");
+const backdrop = document.getElementById("backdrop");
+const openMenu = () => {
+    sideNav.classList.remove("close");
+    hamburger.style.display = "none";
+    backdrop.style.opacity = "1000";
+};
+const closeMenu = () => {
+    sideNav.classList.add("close");
+    hamburger.style.display = "block";
+    backdrop.style.opacity = "0";
+};
+hamburger.addEventListener("click", openMenu);
+closeButton.addEventListener("click", closeMenu);
+backdrop.addEventListener("click", closeMenu);
 const header = document.getElementById("header");
 console.log(header);
 console.log(gsap);
+console.log(header === null || header === void 0 ? void 0 : header.clientHeight);
 const headerTween = gsap
     .timeline({
     scrollTrigger: {
--- scripts/home.ts
+++ scripts/home.ts
@@ -6,8 +6,8 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 const swiperCont = document.querySelector(".mySwiper");
 const valuesSwiper = new Swiper(swiperCont, {
   speed: 700,
-  resistanceRatio: 0.2,
   slidesPerView: "auto",
+  resistanceRatio: 0.2,
   grabCursor: true,
   autoHeight: true,
   spaceBetween: 24,
@@ -15,12 +15,36 @@ const valuesSwiper = new Swiper(swiperCont, {
   keyboard: { enabled: true },
   observer: true,
   observerParents: true,
+  breakpoints: {
+    1100: {
+      slidesPerView: 2.5,
+    },
+  },
 });
 
+const sideNav = document.getElementById("side-nav") as HTMLElement;
+const hamburger = document.getElementById("menu-bar") as HTMLElement;
+const closeButton = document.getElementById("close-button") as HTMLElement;
+const backdrop = document.getElementById("backdrop") as HTMLElement;
+const openMenu = () => {
+  sideNav.classList.remove("close");
+  hamburger.style.display = "none";
+  backdrop.style.opacity = "1000";
+};
+const closeMenu = () => {
+  sideNav.classList.add("close");
+  hamburger.style.display = "block";
+  backdrop.style.opacity = "0";
+};
+
+hamburger.addEventListener("click", openMenu);
+closeButton.addEventListener("click", closeMenu);
+backdrop.addEventListener("click", closeMenu);
 
 const header = document.getElementById("header");
 console.log(header);
 console.log(gsap);
+console.log(header?.clientHeight);
 
 const headerTween = gsap
   .timeline({
